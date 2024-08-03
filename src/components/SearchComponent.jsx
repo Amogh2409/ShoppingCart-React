baaa99334f02ac5564d001ea0044e545f9f2d15f
Amogh Saxena <amoghsaxena24@gmail.com>
2024-08-03 09:22:33 +0530

    Jsx Creation

---
+++ src/components/SearchComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SearchComponent({ searchCourse, courseSearchUserFunction }) {
+    return (
+        <header className="App-header">
+            <h1>GFG Shopping Cart</h1>
+            <div className="search-bar">
+                <input type="text" placeholder="Search for GFG Products....." value={searchCourse} onChange={courseSearchUserFunction} />
+            </div>
+        </header>
+    )
+}
+
+export default SearchComponent;
